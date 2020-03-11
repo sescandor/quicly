8ac8ca1a732573c86c45250d79b87ba2104a7e5b
Sandra O'Keefe <sokeefe@fastly.com>
2020-03-11 13:10:21 -0230

    Add protocol fuzzer

---
+++ fuzz/protocol/fuzz_quicly_prot.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+
+import subprocess
+from scapy.all import *
+
+def mutate(payload):
+	try:
+		cmd = "/Users/sokeefe/Projects/learning/radamsa/bin/radamsa -n 1 -"
+		ps = subprocess.Popen(cmd,shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+		mutated = ps.communicate(payload)[0]
+	except Exception as e:
+		print("Could not execute 'radamsa'." + str(e))
+		sys.exit(1)
+
+	return mutated
+
+def check_process():
+	try:
+		cmd = "ps aux | grep \"./cli -c server.crt -k server.key -vv 0.0.0.0 4433\" | grep -v \"grep\""
+		ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+		output = ps.communicate()[0]		
+		if len(output) == 0:
+			print("quicly server has stopped running. Stopping fuzzing now.")
+			sys.exit(0)
+	except Exception as e:
+		print("Could not get process information for quicly server")
+
+if __name__ == "__main__":
+
+	while(True):	
+		c=rdpcap("packet.pcapng")
+		loaded = c[0][Raw].load
+		new_loaded = mutate(bytes(loaded[0:])) 
+		c[0][Raw].load = new_loaded 
+
+		try:
+			sendp(c[0], iface="lo0")
+			check_process()
+		except Exception as e:
+			print("Error sending packet to server")
